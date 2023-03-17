e2797c894e24627d4a8f52c480ea310fb161c916
gao-wentian <2568036095@qq.com>
2023-03-17 15:39:48 +0800

    权限列表页面渲染

--- src/views/newssandbox/right-manage/RightList.js
+++ src/views/newssandbox/right-manage/RightList.js
@@ -1,7 +1,50 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Button, Space, Table, Tag } from 'antd';
+import {
+  DeleteOutlined, EditOutlined
+} from '@ant-design/icons';
+import axios from 'axios';
 export default function RightList() {
+  const [data, setdata] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:2000/rights?_embed=children').then(res => {
+      console.log(res.data);
+      setdata(res.data)
+    })
+  }, [])
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+    },
+    {
+      title: '权限名称',
+      dataIndex: 'label',
+    },
+    {
+      title: '权限路径',
+      dataIndex: 'key',
+      render(key) {
+        return <Tag color='blue'> {key}</Tag>
+      }
+    },
+    {
+      title: '操作',
+      render(row) {
+        return <Space >
+          <Button type="primary" danger shape="round" ghost icon={<DeleteOutlined />} onClick={handleDelelt(row)} />
+          <Button type="primary" shape="round" ghost icon={<EditOutlined />} onClick={handleEdit(row)} />
+        </Space>
+      }
+    }
+  ];
+  const handleDelelt = (row) => {
+    console.log("删除", row);
+  }
+  const handleEdit = (row) => {
+    console.log("编辑", row);
+  }
   return (
-    <div>RightList</div>
+    <Table columns={columns} dataSource={data} pagination  />
   )
 }

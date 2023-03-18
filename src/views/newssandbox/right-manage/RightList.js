624f4a6c8e54b86706c5c857959a88326067e17d
gaowentian <2568036095@qq.com>
2023-03-18 21:33:55 +0800

    权限列表编辑i删除

--- src/views/newssandbox/right-manage/RightList.js
+++ src/views/newssandbox/right-manage/RightList.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Space, Table, Tag, Modal, message } from 'antd';
-import {
-  DeleteOutlined, EditOutlined, ExclamationCircleOutlined
-} from '@ant-design/icons';
-import axios from 'axios';
-const { confirm } = Modal;
+import { Button, Space, Table, Tag, Modal, message, Switch } from 'antd'
+import { DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+import axios from 'axios'
+const { confirm } = Modal
 export default function RightList() {
   const [data, setdata] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:2000/rights?_embed=children').then(res => {
-      res.data[0].children = ''
+  //获取权限列表
+  function dataList() {
+    axios.get('http://localhost:2000/rights?_embed=children').then((res) => {
+      // res.data[0].children = ''
+      res.data.map((item) => {
+        if (item.children && item.children.length === 0) {
+          item.children = ''
+        }
+        return item
+      })
       setdata(res.data)
     })
+  }
+  useEffect(() => {
+    dataList()
   }, [])
   const columns = [
     {
@@ -26,19 +34,36 @@ export default function RightList() {
       title: '权限路径',
       dataIndex: 'key',
       render(key) {
-        return <Tag color='blue'> {key}</Tag>
-      }
+        return <Tag color="blue"> {key} </Tag>
+      },
     },
     {
       title: '操作',
       render(row) {
-        return <Space >
-          <Button type="primary" danger shape="round" ghost icon={<DeleteOutlined />} onClick={() => handleDelelt(row)} />
-          <Button type="primary" shape="round" ghost icon={<EditOutlined />} onClick={() => handleEdit(row)} />
-        </Space>
-      }
-    }
-  ];
+        return (
+          <Space>
+            <Button
+              type="primary"
+              danger
+              shape="round"
+              ghost
+              icon={<DeleteOutlined />}
+              onClick={() => handleDelelt(row)}
+            />
+
+            <Switch
+              checkedChildren="开启"
+              unCheckedChildren="关闭"
+              checked={row.pagepermisson === 1}
+              onChange={() => handleChange(row)}
+              disabled={row.pagepermisson !== 1}
+            />
+          </Space>
+        )
+      },
+    },
+  ]
+  // 删除
   const handleDelelt = (row) => {
     confirm({
       title: '是否删除当前数据?',
@@ -46,21 +71,43 @@ export default function RightList() {
       okText: '确认',
       cancelText: '取消',
       onOk() {
-        
-        message.success('删除成功');
+        if (row.grade === 1) {
+          // 删除父节点的数据
+          axios.delete(`http://localhost:2000/rights/${row.id}`)
+        } else {
+          // 删除子节点的数据
+          axios.delete(`http://localhost:2000/children/${row.id}`)
+        }
+        dataList()
+        message.success('删除成功')
       },
       onCancel() {
-        message.error('取消删除');
+        message.error('取消删除')
       },
-    });
+    })
   }
-  const handleEdit = (row) => {
-    console.log("编辑", row);
+  // 页面开关
+  const handleChange = (row) => {
+    row.pagepermisson = row.pagepermisson === 1 ? 0 : 1
+    setdata([...data])
+    console.log('编辑', row.pagepermisson)
+    if (row.grade === 1) {
+      // 修改父节点的数据
+      axios.patch(`http://localhost:2000/rights/${row.id}`).then((res) => {
+        // console.log('111111', res)
+        res.data.pagepermisson = row.pagepermisson
+      })
+    } else {
+      // 修改子节点的数据
+      axios.patch(`http://localhost:2000/children/${row.id}`).then((res) => {
+        res.data.pagepermisson = row.pagepermisson
+      })
+    }
+    dataList()
   }
   return (
     <div>
       <Table columns={columns} dataSource={data} pagination />
     </div>
-
   )
 }

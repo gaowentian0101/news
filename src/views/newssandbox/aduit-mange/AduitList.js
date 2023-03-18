a3987c3da31e9fdfca415442c6cc20cdfde9fddb
gaowentian <2568036095@qq.com>
2023-03-18 13:36:28 +0800

    页面架构搭建

---
+++ src/views/newssandbox/aduit-mange/AduitList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AduitList() {
+  return (
+    <div>AduitList</div>
+  )
+}
--- src/views/newssandbox/newssandbox.js
+++ src/views/newssandbox/newssandbox.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
+import "./newssandbox.css"
 import SideMenu from '../../components/sandBox/SideMenu'
 import TopNav from '../../components/sandBox/TopNav'
 import Home from './home/Home'
@@ -7,8 +8,15 @@ import RightList from './right-manage/RightList'
 import RoleList from './right-manage/RoleList'
 import UserList from './user-manage/UserList'
 import Notfound from "./notfound/Notfound"
+import Add from "./news-mange/Add"
 import { Layout } from 'antd';
-import "./newssandbox.css"
+import Draft from "./news-mange/Draft"
+import Category from "./news-mange/Category"
+import Aduit from "./aduit-mange/Aduit"
+import AduitList from "./aduit-mange/AduitList"
+import Unpublished from "./publish-mange/Unpublished"
+import Published from "./publish-mange/Published"
+import Sunset from "./publish-mange/Sunset"
 const { Content } = Layout;
 export default function newssandbox() {
     return (
@@ -29,6 +37,15 @@ export default function newssandbox() {
                         <Route path='/user-manage/list' component={UserList}></Route>
                         <Route path='/right-manage/role/list' component={RoleList}></Route>
                         <Route path='/right-manage/right/list' component={RightList}></Route>
+                        <Route path='/news-manage/add' component={Add}></Route>
+                        <Route path='/news-manage/draft' component={Draft}></Route>
+                        <Route path='/news-manage/category' component={Category}></Route>
+                        <Route path='/audit-manage/audit' component={Aduit}></Route>
+                        <Route path='/audit-manage/list' component={AduitList}></Route>
+                        <Route path='/publish-manage/unpublished' component={Unpublished}></Route>
+                        <Route path='/publish-manage/published' component={Published}></Route>
+                        <Route path='/publish-manage/sunset' component={Sunset}></Route>
+
                         <Redirect from='/' to='/home' exact></Redirect>
                         <Route path='*' component={Notfound}></Route>
                     </Switch>
